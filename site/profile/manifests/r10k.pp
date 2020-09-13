0e26e427008706bcd12a15134f8d5811c279bfbd
altoboxen <altoboxen@gmail.com>
2020-09-12 23:32:16 -0500

    Update r10k.pp

--- site/profile/manifests/r10k.pp
+++ site/profile/manifests/r10k.pp
@@ -3,7 +3,6 @@ class profile::r10k {
     remote => 'https://github.com/altoboxen/control-repo',
   }
   
-  # This is info
   class {'r10k::webhook::config':
     use_mcollective => false,
     enable_ssl      => true,
